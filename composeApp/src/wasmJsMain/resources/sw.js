7bf16a93de8c0ae76ee9f2dff49a3ed53c8e9199
GreenTrackerApp <zacheja17@gmail.com>
2026-07-26 17:05:21 +0200

    Force PWA Cache Refresh (Pro v4)

--- composeApp/src/wasmJsMain/resources/sw.js
+++ composeApp/src/wasmJsMain/resources/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'greentracker-v3';
+const CACHE_NAME = 'greentracker-pro-v4';
 const ASSETS = [
   './',
   './index.html',
@@ -9,11 +9,29 @@ const ASSETS = [
   './manifest.json'
 ];
 
+// Force update on install
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
     })
+  );
+});
+
+// Clean up old caches on activate
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Clearing old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
   );
 });
 
